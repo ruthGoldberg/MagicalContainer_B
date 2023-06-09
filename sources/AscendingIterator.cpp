1d0cf1bbcc99b3c04ee1437ced4380f07cfbaf24
ruthGoldberg <118611563+ruthGoldberg@users.noreply.github.com>
2023-06-09 14:45:46 +0300

    Add files via upload

---
+++ sources/AscendingIterator.cpp
@@ -0,0 +1,34 @@
+#include "MagicalContainer.hpp"
+#include "iostream"
+using namespace ariel;
+using namespace std;
+
+
+MagicalContainer::AscendingIterator::AscendingIterator(const MagicalContainer& container , size_t index):
+Iterator(container,index){}
+
+MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator& iterator):
+Iterator(iterator.container , iterator.index)
+{}
+
+
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator:: begin(){
+    return AscendingIterator(this->container);
+}
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator:: end(){
+
+    return AscendingIterator(this->container , this->container.size());
+    
+}
+MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator++(){
+    if(end().getIndex() == index) 
+        throw runtime_error("you have reached the limit");
+    ++index;
+    return *this;
+}
+int MagicalContainer::AscendingIterator:: operator*(){
+    return container.magicContainer[index];
+}
+
+
+
